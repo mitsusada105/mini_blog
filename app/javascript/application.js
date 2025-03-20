a0e9da240ef63aec9ef07f12f08d9f5e92b5bc1c
mitsusada105 <mitsusada105@gmail.com>
2025-03-20 11:30:52 +0900

    3.bootstrap導入

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,19 @@
 import "@hotwired/turbo-rails"
 import "controllers"
 import "bootstrap"
+import "bootstrap/dist/css/bootstrap.min.css"
+
+
+
+// Bootstrap のタブ機能を有効化
+document.addEventListener("DOMContentLoaded", function() {
+  let triggerTabList = [].slice.call(document.querySelectorAll('a[data-bs-toggle="tab"]'))
+  triggerTabList.forEach(function (triggerEl) {
+    let tabTrigger = new bootstrap.Tab(triggerEl)
+
+    triggerEl.addEventListener("click", function (event) {
+      event.preventDefault()
+      tabTrigger.show()
+    })
+  })
+});
