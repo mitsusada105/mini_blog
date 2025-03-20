98925b89fe955654fff319b20378f92eff54ada0
mitsusada105 <mitsusada105@gmail.com>
2025-03-20 12:22:06 +0900

    3.bootstrap周り修正

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,10 +1,7 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
-import "controllers"
+import "@popperjs/core"
 import "bootstrap"
-import "bootstrap/dist/css/bootstrap.min.css"
-
-
+import "controllers"
 
 // Bootstrap のタブ機能を有効化
 document.addEventListener("DOMContentLoaded", function() {
