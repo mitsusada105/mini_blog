c86b2d4bc987af3ee0e31ae36c58f40877e9b3f5
mitsusada105 <mitsusada105@gmail.com>
2025-03-25 08:39:33 +0900

    bulletをテスト環境で無効にする

--- config/initializers/bullet.rb
+++ config/initializers/bullet.rb
@@ -1,3 +1,5 @@
-# config/initializers/bullet.rb
-
-Bullet.add_safelist type: :unused_eager_loading, class_name: 'Post', association: :likes
+if defined?(Bullet) && Rails.env.development?
+  Bullet.enable = true
+  Bullet.bullet_logger = true
+  Bullet.add_safelist type: :unused_eager_loading, class_name: 'Post', association: :likes
+end
