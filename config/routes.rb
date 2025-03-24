c60d4fcd2feaf0455c3d2e88d66cf68895b28984
mitsusada105 <mitsusada105@gmail.com>
2025-03-24 10:48:41 +0900

    メール確認用にletter_opener_webを追加

--- config/routes.rb
+++ config/routes.rb
@@ -27,4 +27,8 @@ Rails.application.routes.draw do
   end
   resources :relationships, only: [:create, :destroy]
 
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end  
+
 end
