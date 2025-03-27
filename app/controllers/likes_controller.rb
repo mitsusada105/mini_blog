9044e976ebb7f38d1e1233dfd9b666ed504c3335
mitsusada105 <mitsusada105@gmail.com>
2025-03-27 16:52:03 +0900

    リファクタリング

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,28 +1,25 @@
 class LikesController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_post
 
   def create
-    @post = Post.find(params[:post_id])
-    @like = @post.likes.find_or_create_by(user: current_user)
-
-    respond_to do |format|
-      format.turbo_stream do
-        render turbo_stream: [
-          turbo_stream.replace("post_like_#{@post.id}", partial: "posts/post_content_like", locals: { post: @post }),
-          turbo_stream.replace("post_liked_users", partial: "posts/liked_users", locals: { post: @post })
-        ]
-      end
-    end
+    @post.likes.find_or_create_by(user: current_user)
+    render_likes_update
   end
 
   def destroy
+    like = @post.likes.find_by(user: current_user)
+    like&.destroy
+    render_likes_update
+  end
+
+  private
+
+  def set_post
     @post = Post.find(params[:post_id])
-    @like = @post.likes.find_by(user: current_user)
+  end
 
-    if @like
-      @like.destroy
-    end
-
+  def render_likes_update
     render turbo_stream: [
       turbo_stream.replace("post_like_#{@post.id}", partial: "posts/post_content_like", locals: { post: @post }),
       turbo_stream.replace("post_liked_users", partial: "posts/liked_users", locals: { post: @post })
